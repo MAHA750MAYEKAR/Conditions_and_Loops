1450d80a9b4faf94cc9c0980f44343743d3ce85a
MAHANANDA MAYEKAR <152979042+MAHA750MAYEKAR@users.noreply.github.com>
2024-01-22 20:47:41 +0530

    complete assignment

---
+++ Q2.js
@@ -0,0 +1,14 @@
+let marks=100
+if (marks>90) {
+    console.log("Student gets A grade");
+    
+}
+else if(marks>70 && marks<90){
+    console.log("Student gets B grade");
+}
+else if(marks>50 && marks<70){
+    console.log("Student gets C grade");
+}
+else if(marks<50 ){
+    console.log("Student gets F grade");
+}
---
+++ Q5.js
@@ -0,0 +1,14 @@
+//ASCENDING ORDER
+
+let i = 1;
+while (i <= 25) {
+    console.log(i);
+    i++;
+}
+
+
+let j = 25;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
